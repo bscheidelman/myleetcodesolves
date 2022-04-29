50b973dad1b4329f1ac2c13ede9528a099d470db
Ben Scheidelman <55633516+bscheidelman@users.noreply.github.com>
2022-04-29 18:28:54 -0500

    Update ProblemTwo(INCOMPLETE).py

--- ProblemTwo(INCOMPLETE).py
+++ ProblemTwo(INCOMPLETE).py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #convert the object's into two differnt lists
         li1 = []
         li2 = []
         for pair in [[l1,li1],[l2,li2]]:
@@ -16,21 +17,25 @@ class Solution:
                     pair[0] = pair[0].next
                 else:
                     pair[0] = None
-
+        
+        #make the lists equal lengths by adding 0's to the shorter one
         lengthlist = [len(li1),len(li2)]
         alt = [li1,li2]
         while len(li1) != len(li2):
             alt[lengthlist.index(min(lengthlist))].append(0)
         
+        #create a summation of the two lists
         sumli = []
         for val in range(len(li1)):
             sumli.append(li1[val] + li2[val])
         
+        #carry over values to the right if values are >= 10
         for val in range(len(sumli)-1):
             if sumli[val] >= 10:
                 sumli[val+1] += 1
                 sumli[val] -= 10
-
+        
+        #add a new value to the end if the last value is >= 10
         if sumli[-1] >= 10:
             sumli[-1] -= 10
             sumli.append(1)
