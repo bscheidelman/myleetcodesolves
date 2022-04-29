04f45a639662191db1ef3c9783644ec227c846d4
Ben Scheidelman <55633516+bscheidelman@users.noreply.github.com>
2022-04-29 18:33:03 -0500

    Update p12.py

--- p12.py
+++ p12.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def intToRoman(self, num):
+        #Runtime: 46 ms
+        #Memory: 13.5 MB
+        
+        output = ""
+        li = [[1000,"M"],[900,"CM"],[500,"D"],[400,"CD"],[100,"C"],[90,"XC"],[50,"L"],[40,"XL"],[10,"X"],[9,"IX"],[5,"V"],[4,"IV"],[1,"I"]]
+        for pair in li:
+            while num >= pair[0]:
+                output += pair[1]
+                num -= pair[0]
+        return output
